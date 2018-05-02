c351b2b8862b2385092afd941e0aec3406f7a51a
yorkHan <393900482@qq.com>
2018-05-02 14:35:42 +0800

    确定哪些钩子里可以setState

--- src/Welcome.js
+++ src/Welcome.js
@@ -3,17 +3,16 @@ class Welcome extends React.Component{
     constructor(props){
         super(props)
         this.state={
-            data:new Date()
+            data:new Date(),
+            test: '1'
         }
-        setInterval(()=>{
-            this.setState({
-                data:new Date()
-            })
-        })
-
     console.log('我已经在 constructor 里将 props 和 state 初始化好了')
 }
 componentWillMount(){
+    this.setState({
+        data:new Date(),
+        test:'componentWillMount'
+    })
     console.log('运行到这里的话，说明马上就要运行 render 了')
 }
     render() {
@@ -22,10 +21,15 @@ componentWillMount(){
             <div>
                 <h1>Hello,{this.props.name}</h1>
                 <h2>{this.state.data.toString()}</h2>
+                <p>{this.state.test}</p>
             </div>
         )
     }
 componentDidMount(){
+    this.setState({
+        data:new Date(),
+        test:'componentDidMount'
+    })
     console.log('已经挂载到页面里了')
   }
 }
