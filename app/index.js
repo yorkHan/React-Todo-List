4907a2cdd9ab2aa1f4542a95fbc8d074744cf341
yorkHan <393900482@qq.com>
2018-05-02 10:56:23 +0800

    添加 foo.js

--- app/index.js
+++ app/index.js
@@ -1,8 +1,11 @@
 import xxx from 'lodash'
 import $ from 'jquery'
+import foo from './foo'
 function component(){
     var element=$('<div></div>')
     element.html(xxx.join(['hello','world'],' '))
     return element.get(0);
 }
 document.body.appendChild(component());
+console.log(foo)
+console.log(foo())
