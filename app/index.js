9f3886b5e7370e4096b037e6d67151beb58fb740
yorkHan <393900482@qq.com>
2018-05-02 11:28:13 +0800

    使用webpack-dev-server

--- app/index.js
+++ app/index.js
@@ -3,7 +3,7 @@ import $ from 'jquery'
 import foo from './foo'
 function component(){
     var element=$('<div></div>')
-    element.html(xxx.join(['hello','world'],' '))
+    element.html(xxx.join(['hello','kevin'],' '))
     return element.get(0);
 }
 document.body.appendChild(component());
