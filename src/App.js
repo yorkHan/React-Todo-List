61f22c2666b4c0aa31d214b9653ae519120b5e91
yorkHan <393900482@qq.com>
2018-05-03 11:37:53 +0800

    删除leancloud测试代码

--- src/App.js
+++ src/App.js
@@ -16,14 +16,6 @@ AV.init({
   appKey: APP_KEY
 });
 
-var TestObject = AV.Object.extend('TestObject');
-var testObject = new TestObject();
-testObject.save({
-  words: 'Hello World!'
-}).then(function(object) {
-  alert('LeanCloud Rocks!');
-})
-
 class App extends Component {
   constructor(props){
     super(props)
