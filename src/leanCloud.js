b8236ce12abc473eb59e858d717425a46256bf88
yorkHan <393900482@qq.com>
2018-05-03 11:39:56 +0800

    新建leanCloud代码

---
+++ src/leanCloud.js
@@ -0,0 +1,11 @@
+import AV from 'leancloud-storage'
+
+var APP_ID = 'M9QQLN7KtQwtcCgoeHKCxf0X-gzGzoHsz';
+var APP_KEY = 'bQvAndiaTFoIlQgWpL2mWabK';
+
+AV.init({
+  appId: APP_ID,
+  appKey: APP_KEY
+});
+
+export default AV
