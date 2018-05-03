92bc5a2a7d60e70cb6ed7d488282f67671a6ebe3
yorkHan <393900482@qq.com>
2018-05-03 12:24:25 +0800

    切换登录/注册

--- src/UserDialog.js
+++ src/UserDialog.js
@@ -2,22 +2,33 @@ import React, { Component } from 'react';
 import './UserDialog.css'
 
 export default class UserDialog extends Component{
+    constructor(props){
+        super(props)
+        this.state={
+            selected:'signUp'
+        }
+    }
+    switch(e) {
+        this.setState({
+            selected:e.target.value
+        })
+    }
     render(){
         return(
             <div className="UserDialog-Wrapper">
                 <div className="UserDialog">
-                    <nav>
-                        <input type="radio" /> 注册
-                        <input type="radio" /> 登录
+                    <nav onChange={this.switch.bind(this)}>
+                        <input type="radio" value="signUp" checked={this.state.selected==='signUp'}/> 注册
+                        <input type="radio" value="signIn" checked={this.state.selected==='signIn'}/> 登录
                     </nav>
                     <div className="panes">
                         <form class="signUp">{}
                             <div className="row">
-                                <label>用户名<label>
+                                <label>用户名</label>
                                 <input type="text"/>
                             </div>
                             <div className="row">
-                                <label>密码<label>
+                                <label>密码</label>
                                 <input type="password"/>
                             </div>
                             <div className="row actions">
@@ -26,11 +37,11 @@ export default class UserDialog extends Component{
                         </form>
                         <form class="signIn">{}
                             <div className="row">
-                                <label>用户名<label>
+                                <label>用户名</label>
                                 <input type="text"/>
                             </div>
                             <div className="row">
-                                <label>密码<label>
+                                <label>密码</label>
                                 <input type="password"/>
                             </div>
                             <div className="row actions">
